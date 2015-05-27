0d973bc511906762789af27ee3b269b6e42519cd
datamadic <xavier@openfin.co>
2015-05-27 14:57:59 -0400

    added a new ico and some updated styles

--- src/javascript/components/main.js
+++ src/javascript/components/main.js
@@ -7,18 +7,7 @@ var React = require('react'),
         _ = require('underscore');
 
 
-var expandedHeight = 560,
-    state = {
-        symbol: 'XJI',
-        name: 'Xavier JavaScript Industries',
-        lastPrice: 41.55,
-        lastVolume: '2,500',
-        detailRef: 41.10,
-        detailDepthBuy: 450,
-        detailDepthSell: 299,
-        time: '9:27:16',
-        expanded: true
-    };
+var expandedHeight = 560;
 
 module.exports = React.createClass({
 	closeWindow: ()=>{
@@ -37,35 +26,35 @@ module.exports = React.createClass({
 		});
 	},
 	toggleExpand: function (){
-		state.expanded = !state.expanded;// = state.set('expanded', !state.expanded);
+		window.updateStream.expanded = !window.updateStream.expanded;// = state.set('expanded', !state.expanded);
 
 		fin.desktop.main(()=>{
 			fin.desktop.Window.getCurrent()
 				.animate({
 					size: {
-						height: state.expanded ? 560 : 300,
+						height: window.updateStream.expanded ? 560 : 300,
 						duration: 1000
 					}
 				},{},()=>{
-                    this.setState(Immutable.Map(state));
+                    //this.setState(Immutable.Map(state));
                 });
 		});
 	},
   getInitialState: function () {
-  	return state;
+  	return window.updateStream;
   },
   componentDidMount: function() {
       var that = this;
       Object.observe(window.updateStream, _.throttle(() => {
           that.setState(window.updateStream);
-      }, 100));
+      }, 1000));
   },
 	render: function(){
         var cx = classSet,
             iconClasses = classSet({
                 'fa': true,
-                'fa-plus-square': this.state.expanded,
-                'fa-minus-square': !this.state.expanded
+                'fa-plus-square': !this.state.expanded,
+                'fa-minus-square': this.state.expanded
             });
 
 		return	<div className="ls-window">
@@ -85,18 +74,18 @@ module.exports = React.createClass({
             <div className="dashboard">
                 <div className="last">
                     <div className="price">
-                        {this.state.lastPrice}
+                        {this.state.last_price}
                     </div>
                     <div className="volume">
-                        {this.state.lastVolume}
+                        {Number(this.state.last_qty || 0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                     </div>
                 </div>
                 <div className="time">
-                	<span>{this.state.time}</span>
+                	<span>{this.state.last}</span>
                 </div>
                 <div className="detail">
                     <div className="label">REF</div>
-                    <div className="val">{this.state.detailRef}</div>
+                    <div className="val">{this.state.reference_price}</div>
                     <div className="label">DEPTH</div>
                     <div className="val">{this.state.detailDepthBuy} | {this.state.detailDepthSell}</div>
                     <div className="expander">
@@ -112,73 +101,4 @@ module.exports = React.createClass({
 	}
 });
 
-if (!fin.desktop.mock) {
-	fin.desktop.main(()=>{
-		new fin.desktop.Window({
-			name: 'mock',
-			url: 'mock.html',
-			autoShow: true
-		});
-	});	
-}
 
-
-/*
-<div className="ls-window">
-    <div className="window-control">
-        <i onClick={this.minApp} className="fa fa-minus"></i>
-        <i onClick={this.closeApp} className="fa fa-times"></i>
-    </div>
-    <div className="id-bar">
-    	<div className="ticker">
-    		<div className="symbol">XJI</div>
-    		<div className="status">green-circle</div>
-    	</div>
-    	<div className="name">Xavier JavaScript Industries</div>
-    </div>
-    <div className="dashboard">
-    	
-    </div>
-    <div className="grid"></div>
-</div>
-
-
-
-
-
-
-		<div className={this.state.class}>
-							<div className="banner">
-								<div className="title">
-									{this.state.ticker}
-								</div>
-								<div className="window-control">
-									<i onClick={this.minApp} className="fa fa-minus"></i>
-									<i onClick={this.closeApp} className="fa fa-times"></i>
-								</div>
-							</div>
-							<div className="content">
-								<div className="main">
-									<span className="last" >{this.state.last.toFixed(2)}</span>
-									<span className="percent-change" >+%{rndRange().toFixed(2)}</span>
-
-								</div>
-								<div className="pricing">
-									<div className="price open">
-										<div className="label">OPEN</div>
-										<span className="value">{ (this.state.last - rndRange()).toFixed(2) } </span>
-									</div>
-									<div className="price high">
-										<div className="label">HIGH</div>
-										<span className="value">{ (this.state.last + rndRange()).toFixed(2) }</span>
-									</div>
-									<div className="price low">
-										<div className="label">LOW</div>
-										<span className="value">{ (this.state.last - rndRange() - 1).toFixed(2)  }</span>
-									</div>
-								</div>
-							</div>
-						</div>
-
-
-*/
